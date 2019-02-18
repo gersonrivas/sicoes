9453a1d6c9180414c355f9532041627e048868e0
gerson <gerson@gr-laptop>
2019-02-17 21:49:14 -0400

    Adicionando el módulo de Graduados.

--- src/java/com/iupg/sicoes/modelo/DaoGraduados.java
+++ src/java/com/iupg/sicoes/modelo/DaoGraduados.java
@@ -36,6 +36,10 @@ public class DaoGraduados extends Graduados {
             " graduado.cedula, " +
             " alumno.apellidos, " +
             " alumno.nombres, " +
+            " alumno.nacionalidad, " +
+            " alumno.tel_hab, " +
+            " alumno.tel_ofc, " +
+            " alumno.tel_cel, " +
             " graduado.id_especial, " +
             " especialidad.nomb_espec, " +                
             " graduado.id_turno, " +
@@ -60,7 +64,7 @@ public class DaoGraduados extends Graduados {
             " ON (especialidad.\"Id\" = graduado.id_especial) " +
             " LEFT OUTER JOIN turno " +
             " ON (turno.\"Id\" = graduado.id_turno) " +
-            "WHERE cedula = '"+ cedula +"';";
+            "WHERE graduado.cedula = '"+ cedula +"';";
         
         //System.out.println(sql);        
         PreparedStatement ps;        
@@ -72,27 +76,129 @@ public class DaoGraduados extends Graduados {
             this.setIdGraduado(Integer.parseInt(rs.getString("Id")));
             this.setApellidos(rs.getString("apellidos"));
             this.setNombres(rs.getString("nombres"));
-            this.setCodEspecialidad(rs.getString("id_espec"));
-            this.setEspecialidad(rs.getString("nomb_espec"));           
+            this.setCodEspecialidad(rs.getString("id_especial"));
+            this.setEspecialidad(rs.getString("nomb_espec"));
             this.setTurno(rs.getString("nomb_turno"));
-            this.setCodTurno(rs.getString("id_turno"));
-            this.setFecGraduacion(rs.getString("fec_graduacion"));
-            this.setLibro(rs.getString("libro"));
             this.setTomo(rs.getString("tomo"));
-            this.setFolio(rs.getString("folio"));    
-            this.setNumero(rs.getString("numero"));
-            this.setResolucion(rs.getString("resolucion")); 
+            this.setFolio(rs.getString("folio"));
+            this.setNumero(rs.getString("numero")); 
+            this.setResolucion(rs.getString("resolucion"));
             this.setFecActa(rs.getString("fec_acta"));
             this.setFecFirma(rs.getString("fec_firma"));
-            this.setRegGrado(rs.getString("reg_grado"));
-            this.setAranceles(rs.getString("aranceles"));
-            this.setIdentCorreo(rs.getString("ident_correo"));
+            this.setFecGraduacion(rs.getString("fec_graduacion"));
             this.setSede(rs.getString("sede"));
                         
             return true;
         } else {
             return false;
         }
+    }    
+    
+    
+    public String BuscarDatosGraduado(Connection conexion, String editar) throws SQLException {
+        String sql;
+        String detalleGraduado = "";
+        String editarOtros="";
+        
+        if (editar=="readonly") {
+            editarOtros="disabled";            
+        }
+        
+        sql = "SELECT " +
+            " graduado.\"Id\", " +
+            " graduado.cedula, " +
+            " alumno.apellidos, " +
+            " alumno.nombres, " +
+            " alumno.nacionalidad, " +
+            " alumno.tel_hab, " +
+            " alumno.tel_ofc, " +
+            " alumno.tel_cel, " +
+            " graduado.id_especial, " +
+            " especialidad.nomb_espec, " +                
+            " graduado.id_turno, " +
+            " turno.nomb_turno, " +
+            " graduado.fec_graduacion, " +
+            " graduado.libro, " +
+            " graduado.tomo, " +
+            " graduado.folio, " +
+            " graduado.numero, " +
+            " graduado.resolucion, " +
+            " graduado.fec_acta, " +
+            " graduado.fec_firma, " +
+            " graduado.reg_grado, " +
+            " graduado.aranceles, " +
+            " graduado.ident_correo, " +
+            " graduado.sede " +
+            " FROM " +
+            " public.graduado " +
+            " INNER JOIN public.alumno " +
+            " ON (alumno.cedula = graduado.cedula) " +
+            " LEFT OUTER JOIN especialidad " +
+            " ON (especialidad.\"Id\" = graduado.id_especial) " +
+            " LEFT OUTER JOIN turno " +
+            " ON (turno.\"Id\" = graduado.id_turno) " +
+            "WHERE graduado.cedula = '"+ cedula +"';";
+        
+        //System.out.println(sql);        
+        PreparedStatement ps;        
+        ps = conexion.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        
+        if (rs.next()) {
+            
+            detalleGraduado = "<tr>"+
+                                "<td align=\"right\"> Cédula: </td> <td><input type=\"text\" name=\"fcedula\" value=  \""+rs.getString("cedula")+"\" disabled></td>" +
+                                "<td align=\"right\"> Apellidos: </td> <td><input type=\"text\" name=\"fapellidos\" value=  \""+rs.getString("apellidos")+"\" disabled></td>" +
+                                "<td align=\"right\"> Nombres: </td> <td><input type=\"text\" name=\"fnombres\" value=  \""+rs.getString("nombres")+"\" disabled></td>" +
+                              "<tr>" +
+                              
+                              "<tr>"+
+                                "<td align=\"right\"> Nacionalidad: </td> <td><input type=\"text\" name=\"fnacionalidad\" value=  \""+rs.getString("nacionalidad")+"\" disabled></td>" +
+                                "<td align=\"right\"> Correo: </td> <td><input type=\"text\" name=\"fident_correo\" value=  \""+rs.getString("ident_correo")+"\" disabled></td>" +
+                                "<td align=\"right\"> Tel. Hab.: </td> <td><input type=\"text\" name=\"ftel_hab\" value=  \""+rs.getString("tel_hab")+"\" disabled></td>" +
+                              "<tr>" +
+                    
+                              "<tr>" +
+                                "<td align=\"right\"> Tel. Ofc.: </td> <td><input type=\"text\" name=\"ftel_ofc\" value=  \""+rs.getString("tel_ofc")+"\" disabled></td>" +
+                                "<td align=\"right\"> Tel. Cel.: </td> <td><input type=\"text\" name=\"ftel_cel\" value=  \""+rs.getString("tel_cel")+"\" disabled></td>" +
+                                "<td align=\"right\"> Fecha Graduación: </td> <td><input type=\"text\" name=\"ffec_graduacion\" value=  \""+rs.getString("fec_graduacion")+"\" disabled></td>" +
+                              "<tr>" +
+                    
+                            "<tr> " +
+                                "<td align=\"right\">  Sede:  </td> <td> <input type=\"text\" name=\"fsede\" value = \"" +rs.getString("sede") + "\" disabled > </td> " +
+                                "<td align=\"right\"> Libro: </td> <td> <input type=\"text\" name=\"flibro\" value = \"" +rs.getString("libro") + "\" " + editar + " maxlength=\"10\" > </td> " +
+                                "<td align=\"right\"> Tomo: </td> <td> <input type=\"text\" name=\"ftomo\" value = \"" +rs.getString("tomo") + "\" " + editar + " maxlength=\"10\" > </td> " +                                
+                            "</tr> " +
+                            
+                            "<tr> " +
+                                "<td align=\"right\"> Folio: </td> <td> <input type=\"text\" name=\"ffolio\" value = \"" +rs.getString("folio") + "\" " + editar + " maxlength=\"9\" onkeypress=\"return checkIt(event);\" > </td> " +
+                                "<td align=\"right\"> Número: </td> <td> <input type=\"text\" name=\"fnumero\" value = \"" +rs.getString("numero") + "\" " + editar + " maxlength=\"10\" > </td> " +
+                                "<td align=\"right\"> Resolución: </td> <td> <input type=\"text\" name=\"fresolucion\" value = \"" +rs.getString("resolucion") + "\" " + editar + " maxlength=\"10\" > </td> " +
+                            "</tr>  " +
+                    
+                            "<tr> " +
+                                "<td align=\"right\"> Fecha Acta: </td> <td> <input type=\"date\" name=\"ffec_acta\" value = \"" +rs.getString("fec_acta") + "\" " + editar + " > </td> " +
+                                "<td align=\"right\">  Fecha Firma:  </td> <td> <input type=\"date\" name=\"ffec_firma\" value = \"" +rs.getString("fec_firma") + "\" " + editar + " > </td> ";
+            
+            if (rs.getString("reg_grado").equals("t")) {
+                            detalleGraduado = detalleGraduado + "<td align=\"right\"> Registro Grado: </td> <td> <input type=\"checkbox\" name=\"freg_grado\" value = \"" +rs.getString("reg_grado") + "\" " + editarOtros + " checked > </td> ";
+            } else {
+                            detalleGraduado = detalleGraduado + "<td align=\"right\"> Registro Grado: </td> <td> <input type=\"checkbox\" name=\"freg_grado\" value = \"" +rs.getString("reg_grado") + "\" " + editarOtros + "  > </td> ";
+            }
+            System.err.println("rs.getString(\"reg_grado\")"+rs.getString("reg_grado"));
+                                
+            detalleGraduado = detalleGraduado + "</tr> " +
+                    
+                            "<tr> " +
+                                "<td align=\"right\"> Aranceles: </td> <td> <input type=\"text\" name=\"faranceles\" value = \"" +rs.getString("aranceles") + "\" " + editar + " maxlength=\"50\" > </td> " +
+                                "<td> </td> <td> </td> " +
+                                "<td> </td> <td> </td> " +
+                            "</tr> ";
+                    
+
+        }
+        //System.out.println(detalleGraduado);
+        return detalleGraduado;
     }
 
 
@@ -155,7 +261,7 @@ public class DaoGraduados extends Graduados {
                                 "  <b> TURNO </b> " +
                                 "</th> " +
                                 "<th align=\"center\"> " +
-                                "  <b> DETALLE </b> " +
+                                "  <b> ESTATUS </b> " +
                                 "</th> " +
                             "</tr>";
         
@@ -177,47 +283,14 @@ public class DaoGraduados extends Graduados {
                                     "<td align=\"center\"> " +
                                         rs.getString("nomb_turno") +
                                     "</td>" +
-                                    "<td align=\"center\"> " +
-                                        "Ver" +
-                                    "</td>" +
+                                    "<td align=\"center\"><a href=\"xgraduadoDetalle.do?cedulaGraduadoSession="+rs.getString("cedula")+"\"> Ver </a></td>"+ 
                                 "</tr>";
-                    
                         
         }
         return listado;
     }
 
-    
-    public String BuscarPeriodos(Connection conexion)  throws SQLException {
-        String sql;
-        String selectPeriodo = "";
-        String optionsPeriodo = "";
-        int i=0;
-        
-        sql = " SELECT DISTINCT " +
-            " inscripcion_alumno.periodo " +
-            " FROM " +
-            " public.inscripcion_alumno; ";
-        
-        //System.out.println(sql);
-        
-        PreparedStatement ps;        
-        ps = conexion.prepareStatement(sql);
-        ResultSet rs = ps.executeQuery();
-        
-        selectPeriodo = "<select name=\"periodo\" onchange=\"form.submit()\" id=\"periodo\" >"; 
-        optionsPeriodo =  "<option value=\"lbElige\" selected>Elige";
-        while (rs.next()) {
-            i++;
-            optionsPeriodo = optionsPeriodo + "<option value=\"lb"+rs.getString("periodo")+"\" >"+rs.getString("periodo");
-        }
-        if (i==0) {
-            optionsPeriodo =  "<option value=\"lbSinInscripcion\" selected>Sin Inscripción";
-        }
-        selectPeriodo = selectPeriodo + optionsPeriodo + "</select>";
-
-        return selectPeriodo;
-    }
+   
     
     public String BuscarSede(Connection conexion)  throws SQLException {
         String sql;
@@ -350,4 +423,18 @@ public class DaoGraduados extends Graduados {
 
         return selectSeccion;
     }
+    
+    public String GuardarGraduado(Connection conexion, String sqlInsertUpdate)  throws SQLException, Exception {
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sqlInsertUpdate);
+            int cantidadRegistros = ps.executeUpdate();
+            System.out.println("Cantidad egistros:-->"+cantidadRegistros);
+            conexion.close();
+            return "Se actualió "+cantidadRegistros+" registro Satisfactoriamente.";
+        } catch (SQLException e) {
+            conexion.close();
+            return e.getMessage();
+        }
+    }
+    
 }
