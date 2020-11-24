3fdde379e4644cb66c495b64470eedbd512a6f17
gerson <gerson.rivas.21@gmail.com>
2020-11-23 20:52:36 -0400

    Nueas Modificaciones del Módulo de Administración

--- src/java/com/iupg/sicoes/controlador/SeleccionProfesorAsignaturaSeccionController.java
+++ src/java/com/iupg/sicoes/controlador/SeleccionProfesorAsignaturaSeccionController.java
@@ -7,10 +7,28 @@ package com.iupg.sicoes.controlador;
 import com.iupg.sicoes.modelo.DaoProfesor;
 
 import com.iupg.sicoes.modelo.DaoConexion;
+import com.iupg.sicoes.propiedades.ParamConfig;
+import com.iupg.sicoes.servicio.ParamCorreo;
+import com.lowagie.text.pdf.PdfWriter;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
@@ -33,6 +51,10 @@ public class SeleccionProfesorAsignaturaSeccionController extends AbstractContro
         String pagina = "academico/registroControl/seleccionProfesorAsignaturaSeccion";
         HttpSession misession= request.getSession(); 
         daoConexion = new DaoConexion();
+        
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ParamCorreo.class);
+        ParamCorreo pv = ctx.getBean(ParamCorreo.class);
+        String rutaPDFAdjunto = pv.getCorreo_rutaArcAdj()+System.getProperty("file.separator")+"actas_notas";
         
         boolean validado = false;
         
@@ -71,8 +93,8 @@ public class SeleccionProfesorAsignaturaSeccionController extends AbstractContro
                         
                                     if (!"lbElige".equals(request.getParameter("sede")) && (!"lbSinSede".equals(request.getParameter("sede")))) {
                                         if (!"lbElige".equals(request.getParameter("materia")) && (!"lbSinAsignatura".equals(request.getParameter("materia")))) {
-                                            if (!"lbElige".equals(request.getParameter("seccion")) && (!"lbSinSeccion".equals(request.getParameter("seccion")))) {
-                                                misession.setAttribute("botonSession","<input name=\"action\" type=\"submit\" id=\"cargar\" value=\"Cargar\" />");
+                                            if (!"lbElige".equals(request.getParameter("seccion")) && (!"lbSinSeccion".equals(request.getParameter("seccion")))) {                                               
+                                                misession.setAttribute("botonSession","<input name=\"action\" type=\"submit\" id=\"cargar\" value=\"Cargar\" />");                                                
                                             }
                                         }
                                     }
@@ -121,8 +143,13 @@ public class SeleccionProfesorAsignaturaSeccionController extends AbstractContro
                                                 
                                 //if (!"lbElige".equals(request.getParameter("seccion")) && (!"lbSinSeccion".equals(request.getParameter("seccion")))) {
                                     daoProfesor.setSeccion(request.getParameter("seccion").substring(2, request.getParameter("seccion").length()));    
-                        
-                                    misession.setAttribute("botonSession","<input name=\"action\" type=\"submit\" id=\"cargar\" value=\"Cargar\" />");
+                                    
+                                    String tipoUsu = (String)misession.getAttribute("tipoUsuSession");
+                                    if (tipoUsu.equals("S")) {
+                                        misession.setAttribute("botonSession","<input name=\"action\" type=\"submit\" id=\"cargar\" value=\"Cargar\" />");
+                                    } else {
+                                        misession.setAttribute("botonSession","<input name=\"action\" type=\"submit\" id=\"imprimir\" value=\"Imprimir\" />");
+                                    }                                    
                         
                                 //}
                             //}
@@ -165,6 +192,33 @@ public class SeleccionProfesorAsignaturaSeccionController extends AbstractContro
                                     break;
                                 case "Modificar": 
                                     //mensaje = daoGeneral.AdicionarSeccionFiltrada(daoConexion.ConexionBD());                            
+                                    break;
+                                case "Imprimir": 
+                                    //mensaje = daoGeneral.AdicionarSeccionFiltrada(daoConexion.ConexionBD());                            
+                                    // Exportar Acta de Notas
+                                    ServletOutputStream out = response.getOutputStream();
+                                    String actasNotas=ParamConfig.getString("reporte.archivoReporteActasNotasCargada");                                    
+                                    String planillaInscAdj = rutaPDFAdjunto + System.getProperty("file.separator")+"Acta_Notas_" + daoProfesor.getCedula()+"_"+daoProfesor.getPeriodo()+"_"+daoProfesor.getAsignatura()+"_"+daoProfesor.getSeccion()+".pdf";
+                                    File theFile = new File(actasNotas);
+                                    JasperReport reporte = (JasperReport) JRLoader.loadObject(theFile);
+                                    
+                                    Map<String, Object> parametros = new HashMap();
+                                    parametros.put("CEDULA", new Integer(daoProfesor.getCedula()));
+                                    parametros.put("PERIODO", new String(daoProfesor.getPeriodo()));
+                                    parametros.put("SEDE", new String(daoProfesor.getSede()));
+                                    parametros.put("SECCION", new String(daoProfesor.getSeccion()));
+                                    parametros.put("LOGO", new String(ParamConfig.getString("reporte.archivoImagenLogo")));
+                                    parametros.put("MATERIA", new String(daoProfesor.getAsignatura()));
+                                    
+                                    JasperPrint print = JasperFillManager.fillReport(reporte, parametros, daoConexion.ConexionBD());
+
+                                    JRPdfExporter exporter = new JRPdfExporter(); 
+                                    exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+                                    exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
+                                    exporter.exportReport();
+                                    
+                                    
+
                                     break;
                                 case "Salir": {
                                     mensaje = "";
