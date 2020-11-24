d54ed47b2b7121ebc309541f47c5687856248685
gerson <gerson.rivas.21@gmail.com>
2020-11-23 20:55:11 -0400

    Nueas Modificaciones del Módulo de Administración

--- src/java/com/iupg/sicoes/modelo/General.java
+++ src/java/com/iupg/sicoes/modelo/General.java
@@ -24,6 +24,23 @@ public class General {
     protected String hora_ini;
     protected String hora_fin;
     protected String cedula;
+    protected String estatus;
+    protected String periodo_divisas;
+    
+    protected String fec_ini;
+    protected String fec_fin;
+    protected String descrip;
+    protected String fec_ini_insc;
+    protected String fec_fin_insc;
+    protected String fec_ini_raa;
+    protected String fec_fin_raa;
+    protected Integer id_grupo_horario;
+    
+    protected double monto_sem;
+    protected Integer nro_cuotas;
+    protected double valor_divisa;
+    
+    
 
     public String getCedula() {
         return cedula;
@@ -136,4 +153,114 @@ public class General {
     public void setAula(String aula) {
         this.aula = aula;
     }
+    
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getPeriodo_divisas() {
+        return periodo_divisas;
+    }
+
+    public void setPeriodo_divisas(String periodo_divisas) {
+        this.periodo_divisas = periodo_divisas;
+    }
+
+     public String getFec_ini() {
+        return fec_ini;
+    }
+
+    public void setFec_ini(String fec_ini) {
+        this.fec_ini = fec_ini;
+    }
+
+    public String getFec_fin() {
+        return fec_fin;
+    }
+
+    public void setFec_fin(String fec_fin) {
+        this.fec_fin = fec_fin;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public String getFec_ini_insc() {
+        return fec_ini_insc;
+    }
+
+    public void setFec_ini_insc(String fec_ini_insc) {
+        this.fec_ini_insc = fec_ini_insc;
+    }
+
+    public String getFec_fin_insc() {
+        return fec_fin_insc;
+    }
+
+    public void setFec_fin_insc(String fec_fin_insc) {
+        this.fec_fin_insc = fec_fin_insc;
+    }
+
+    public String getFec_ini_raa() {
+        return fec_ini_raa;
+    }
+
+    public void setFec_ini_raa(String fec_ini_raa) {
+        this.fec_ini_raa = fec_ini_raa;
+    }
+
+    public String getFec_fin_raa() {
+        return fec_fin_raa;
+    }
+
+    public void setFec_fin_raa(String fec_fin_raa) {
+        this.fec_fin_raa = fec_fin_raa;
+    }
+
+    public Integer getId_grupo_horario() {
+        return id_grupo_horario;
+    }
+
+    public void setId_grupo_horario(Integer id_grupo_horario) {
+        this.id_grupo_horario = id_grupo_horario;
+    }
+
+    
+    
+    
+    
+    public double getMonto_sem() {
+        return monto_sem;
+    }
+
+    public void setMonto_sem(double monto_sem) {
+        this.monto_sem = monto_sem;
+    }
+
+    public Integer getNro_cuotas() {
+        return nro_cuotas;
+    }
+
+    public void setNro_cuotas(Integer nro_cuotas) {
+        this.nro_cuotas = nro_cuotas;
+    }
+
+    public double getValor_divisa() {
+        return valor_divisa;
+    }
+
+    public void setValor_divisa(double valor_divisa) {
+        this.valor_divisa = valor_divisa;
+    }
+    
+    
 }
